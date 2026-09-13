0c0d13cff27464bc825870424d78a8fa152ca2af
Yassine Agouram <121446365+YassineAgouram@users.noreply.github.com>
2026-09-13 21:01:04 +0100

    Time: 17 ms (47.77%), Space: 12.3 MB (58.59%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -8,10 +8,13 @@ class Solution(object):
         """
         merged = []
         i = 0
-        while i < len(word1) or i < len(word2) :
-           if i < len(word1):
-            merged.append(word1[i])
-           if i < len(word2):
-              merged.append(word2[i])
-           i += 1
-        return "".join(merged)
+        
+
+        while i<len(word1) or i<len(word2):
+            if i<len(word1):
+                merged.append(word1[i])
+            if i<len(word2):
+                merged.append(word2[i])
+            i+=1
+        return "".join(merged)    
+
